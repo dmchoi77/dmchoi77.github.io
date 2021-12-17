3641b2d963d29a15fbe5a6bae66b9a61a71d673d
dmchoi <david5760@naver.com>
2021-12-17 23:07:38 +0900

    Fixed Utterance Widget

--- src/components/Post/CommentWidget.tsx
+++ src/components/Post/CommentWidget.tsx
@@ -2,7 +2,7 @@ import React, { createRef, FunctionComponent, useEffect } from 'react'
 import styled from '@emotion/styled'
 
 const src = 'https://utteranc.es/client.js'
-const repo = 'dmchoi77/gatsby-blog'
+const repo = 'dmchoi77/dmchoi77.github.io'
 
 type UtterancesAttributesType = {
     src: string
